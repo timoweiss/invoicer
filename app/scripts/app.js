cef05a288ca5823df94f31072ca43a35abf9e3f5
Timo Weiß <info@timo-weiss.com>
2014-09-03 18:17:52 +0200

    init 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,30 @@
  * Main module of the application.
  */
 angular
-  .module('invoicePocApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('invoicePocApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/documents', {
+                templateUrl: 'views/documents.html',
+                controller: 'DocumentsCtrl'
+            })
+            .when('/clients', {
+                templateUrl: 'views/clients.html',
+                controller: 'ClientsCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
