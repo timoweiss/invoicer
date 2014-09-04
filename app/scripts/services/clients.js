afbc7ce8795de5d9b4433a3460e740617d668791
Timo Weiß <info@timo-weiss.com>
2014-09-04 16:46:20 +0200

    add saveClients

--- app/scripts/services/clients.js
+++ app/scripts/services/clients.js
@@ -6,6 +6,17 @@ angular.module('invoicePocApp')
     return {
         getClients: function() {
             return $http.get('http://localhost:2403/clients');
+        },
+        saveClient: function(formData) {
+            if (!formData) {
+                throw new TypeError('formData not defined');
+            }
+            if (!formData.id) {
+                return $http.post('http://localhost:2403/clients', formData);
+            } else {
+                return $http.put('http://localhost:2403/clients', formData);
+            }
+
         }
     };
 });
