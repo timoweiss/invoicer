19817c0470699f565490f9b86dd1d7bbeeaf1967
Timo Weiß <info@timo-weiss.com>
2014-09-04 17:09:12 +0200

    fix tabs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
         'ngRoute',
         'ngSanitize',
         'ngTouch',
-        'ui.router'
+        'ui.router',
+        'ngMaterial'
     ])
     .config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise("/");
