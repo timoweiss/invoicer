18fbdc446d862fbae0c938d37ac6ce0c8db61751
Timo Weiß <info@timo-weiss.com>
2014-09-09 17:56:09 +0200

    login scaffold

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,6 +11,8 @@ angular.module('invoicePocApp')
     .controller('LoginCtrl', function($scope, $state) {
         console.log(require('nw.gui'));
         $scope.login = function() {
+            win.width = 1000;
+            win.height = 723;
             $state.go('main');
         };
     });
