5c8e24283e5dda752518f2d83d713921e59c6f52
Michael Knoch <info@michaelknoch.de>
2014-09-11 03:00:36 +0200

    add invoice edit

--- app/scripts/controllers/documents.js
+++ app/scripts/controllers/documents.js
@@ -14,26 +14,73 @@ angular.module('invoicePocApp')
 
         $scope.toggleInvoice = function ($event) {
             var el = $event.currentTarget;
-
-
             $(el).find('.items').toggleClass('show');
         };
 
-        InvoiceService.getInvoices().then(function (respInvoices) {
-            ClientsService.getClients().then(function (respClients) {
-                if (respInvoices.status === 200 || respInvoices.status === 304) {
-                    $scope.invoices = respInvoices.data;
-                    angular.forEach($scope.invoices, function (i) {
-                        angular.forEach(respClients.data, function (c) {
-                            if (c.id === i.client) {
-                                i.client = c;
-                            }
-                        });
+        $scope.tabs = [{
+            title: 'Rechnungen',
+            active: true
+        }, {
+            title: 'Rechnung anlegen',
+            active: false
+        }];
 
-                    });
+        $scope.selectedIndex = 0;
+
+        $scope.editInvoice = function (id) {
+            debugger;
+            angular.forEach($scope.invoices, function (value, key) {
+                if (value.id === id) {
+                    $scope.formdata = value;
+                    $scope.selectedIndex = 1;
                 }
             });
+        };
 
-        });
+        $scope.addItem = function () {
+            $scope.formdata.invoiceContent.push({
+                itemName: '',
+                itemDescription: '',
+                itemUnitcosts: '',
+                itemQuantity: ''
+            });
+        };
+
+        $scope.saveInvoice = function () {
+
+            InvoiceService.saveInvoice($scope.formdata).then(function () {
+                $scope.formdata = {};
+                $scope.selectedIndex = 0;
+                $scope.tabs[1].title = 'Rechnung anlegen';
+            });
+
+
+        };
+
+        $scope.deleteInvoice = function (id) {
+            InvoiceService.deleteInvoice(id).then(function () {
+                getInvoices();
+            });
+        };
+
+        function getInvoices() {
+            InvoiceService.getInvoices().then(function (respInvoices) {
+                ClientsService.getClients().then(function (respClients) {
+                    if (respInvoices.status === 200 || respInvoices.status === 304) {
+                        $scope.invoices = respInvoices.data;
+                        angular.forEach($scope.invoices, function (i) {
+                            angular.forEach(respClients.data, function (c) {
+                                if (c.id === i.client) {
+                                    i.client = c;
+                                }
+                            });
+
+                        });
+                    }
+                });
+
+            });
+        }
+        getInvoices();
 
     });
--- app/scripts/services/invoices.js
+++ app/scripts/services/invoices.js
@@ -2,10 +2,21 @@
 
 angular.module('invoicePocApp')
 
-.service('InvoiceService', function($http) {
+.service('InvoiceService', function ($http) {
     return {
-        getInvoices: function() {
+        getInvoices: function () {
             return $http.get('http://localhost:2403/invoices');
+        },
+        saveInvoice: function (invoice) {
+            if (!invoice.id) {
+                return $http.post('http://localhost:2403/invoices', invoice);
+            } else {
+                return $http.put('http://localhost:2403/invoices', invoice);
+            }
+
+        },
+        deleteInvoice: function (id) {
+            return $http.delete('http://localhost:2403/invoices/' + id);
         }
     };
 });
