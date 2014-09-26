05dfd622bf643fc7b8a35775950cf9a6788df15e
Timo Weiß <info@timo-weiss.com>
2014-09-26 15:37:09 +0200

    set up config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,17 +54,56 @@ angular
             });
     })
     .config(function(configProvider) {
+        var _configMock = {
+            'profile': {
+                'firstname': 'John',
+                'surname': 'Doe',
+                'email': 'john@doe.com',
+                'timezone': '',
+                'image': ''
+            },
+            'preferences': {
+                'askBeforeDelete': true,
+                'undoredo': true
+            },
+            'notifications': {
+                'otherUserOnline': true,
+                'otherUserOffline': true,
+                'updateAvailable': true,
+                'changes': true
+            },
+            'hiddenPreferences': {
+                'windowWith': 1000,
+                'windowHeight': 700,
+                'windowPosition': {
+                    'top': 300,
+                    'left': 300
+                }
+            }
+        };
+
         global.config = configProvider.$get();
-        global.config({
-            data: 'data'
-        }).then(function() {
-            console.log('success');
+        global.config().then(function(data) {
+            console.log('#1 getter', global.noConfig);
+            global.noConfig = true;
+            global.configData = data;
+        }).fail(function(err) {
+            console.log('#1 getter error', global.noConfig);
+            if (err.errno === -2) {
+                global.config(_configMock).then(function() {
+                    debugger;
+                    global.noConfig = true;
+                });
+            }
         });
 
-        global.config().then(function(data) {
-            console.log(data);
-        });
     })
     .config(function(shortcutProvider) {
         var shortcut = shortcutProvider.$get();
+    })
+    .run(function($state) {
+        console.log('#2 run', global.noConfig);
+        if (!global.noConfig) {
+            $state.go('clients');
+        }
     });
